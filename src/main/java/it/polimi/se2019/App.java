7e8b481cddcd9c666a629cbe8c6b347b11985d01
Giuseppe Luzzi <me@giuseppeluzzi.com>
2019-03-19 17:34:52 +0100

    Initial commit

---
+++ src/main/java/it/polimi/se2019/App.java
@@ -0,0 +1,15 @@
+package it.polimi.se2019;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App {
+    
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+}
