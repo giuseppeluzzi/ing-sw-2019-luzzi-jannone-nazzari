22307fbbe62b4143263e783662a30a790f20b433
Jacopo Jannone <jacopo.jannone@jacopojannone.com>
2019-04-13 18:39:32 +0200

    Configuration from file

--- src/main/java/it/polimi/se2019/adrenalina/controller/Configuration.java
+++ src/main/java/it/polimi/se2019/adrenalina/controller/Configuration.java
@@ -1,19 +1,31 @@
 package it.polimi.se2019.adrenalina.controller;
 
+import com.google.gson.Gson;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
 public class Configuration {
   private String serverIP;
+  private Integer rmiPort;
+  private Integer socketPort;
   private Integer joinTimeout;
   private Integer turnTimeout;
+  private Integer powerUpTimeout;
 
-  private static Configuration instance;
+  private static Configuration instance = null;
 
   private Configuration() {
     // private constructor
   }
 
-  public static synchronized Configuration getInstance() {
+  public static synchronized Configuration getInstance() throws IOException {
     if (instance == null) {
-      instance = new Configuration();
+      File file  = new File(Configuration.class.getResource("/config.json").getFile());
+      String json = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+      Gson gson = new Gson();
+      instance = gson.fromJson(json, Configuration.class);
     }
     return instance;
   }
@@ -29,4 +41,17 @@ public class Configuration {
   public Integer getTurnTimeout() {
     return turnTimeout;
   }
+
+  public Integer getRmiPort() {
+    return rmiPort;
+  }
+
+  public Integer getSocketPort() {
+    return socketPort;
+  }
+
+  public Integer getPowerUpTimeout() {
+    return powerUpTimeout;
+  }
+
 }
