7b774d234570fd77e5499b57b5af3ff07e075bcf
Jacopo Jannone <jacopo.jannone@jacopojannone.com>
2019-05-24 16:42:55 +0200

    Changes to SquareColors & fixed map2

--- src/main/java/it/polimi/se2019/adrenalina/controller/SquareColor.java
+++ src/main/java/it/polimi/se2019/adrenalina/controller/SquareColor.java
@@ -1,10 +1,20 @@
 package it.polimi.se2019.adrenalina.controller;
 
 public enum SquareColor {
-  GREEN,
-  BLUE,
-  PURPLE,
-  GREY,
-  YELLOW,
-  RED
+  GREEN("\u001b[32m"),
+  BLUE("\u001b[34m"),
+  PURPLE("\u001b[35m"),
+  GREY("\u001b[37m"),
+  YELLOW("\u001b[33m"),
+  RED("\u001b[31m");
+
+  private final String ansiColor;
+
+  SquareColor(String ansiColor) {
+    this.ansiColor = ansiColor;
+  }
+
+  public String getAnsiColor() {
+    return ansiColor;
+  }
 }
