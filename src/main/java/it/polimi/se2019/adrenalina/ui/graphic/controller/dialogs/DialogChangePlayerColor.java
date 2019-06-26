32ee9f4b070b36d44d117d4271a13a26cc14979e
Giuseppe Luzzi <me@giuseppeluzzi.com>
2019-06-26 15:32:47 +0200

    Fix in DialogChangePlayerColor

--- src/main/java/it/polimi/se2019/adrenalina/ui/graphic/controller/dialogs/DialogChangePlayerColor.java
+++ src/main/java/it/polimi/se2019/adrenalina/ui/graphic/controller/dialogs/DialogChangePlayerColor.java
@@ -6,6 +6,10 @@ import it.polimi.se2019.adrenalina.event.viewcontroller.PlayerColorSelectionEven
 import it.polimi.se2019.adrenalina.model.Player;
 import it.polimi.se2019.adrenalina.utils.Log;
 import it.polimi.se2019.adrenalina.view.BoardView;
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javafx.css.Styleable;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
@@ -18,11 +22,6 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
-
-import java.rmi.RemoteException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class DialogChangePlayerColor extends Dialog {
 
@@ -44,12 +43,12 @@ public class DialogChangePlayerColor extends Dialog {
       try {
         if (AppGUI.getClient().getBoardView().getBoard().getFreePlayerColors()
             .contains(chosenColor)) {
-          build();
-        } else {
-            ((BoardView) AppGUI.getClient().getBoardView()).sendEvent(
-                new PlayerColorSelectionEvent(AppGUI.getClient().getPlayerColor(), chosenColor));
+          ((BoardView) AppGUI.getClient().getBoardView()).sendEvent(
+              new PlayerColorSelectionEvent(AppGUI.getClient().getPlayerColor(), chosenColor));
 
           close();
+        } else {
+          build();
         }
       } catch (RemoteException e) {
         Log.exception(e);
